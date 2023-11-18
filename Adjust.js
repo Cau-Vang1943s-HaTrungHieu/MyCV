a6c833563f54d278fca78386295dec57a3fc822b
Ha Trung Hieu <gooddoggy1509gmail.com>
2023-11-18 17:46:32 +0700

    First commit

---
+++ Adjust.js
@@ -0,0 +1,11 @@
+function openFunction1() {
+    document.getElementById("Skill").style.display = "block";
+}
+
+function openFunction2() {
+    document.getElementById("PronCons").style.display = "block";
+}
+
+function openFunction3() {
+    document.getElementById("Contact").style.display = "block";
+}
